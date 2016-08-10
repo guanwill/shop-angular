b139e3c5328f6280ac6f9a7673698a20cef4e143
guanwill <w.guan@hotmail.com>
2016-08-11 01:38:31 +1000

    added ui-router, states

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
 angular
-  .module("shopApp", ['ngResource']) //grab resources and dependencies
-  .constant('API', 'http://localhost:3000/api/items');
+  .module("shopApp", ['ui.router', 'ngResource']) //grab resources and dependencies
+  .constant('API', 'http://localhost:3000/api/items')
+  .config(MainRouter); //router configuration function defined below
+
+  function MainRouter($stateProvider, $urlRouterProvider){
+	$stateProvider //define diff routes here.
+		.state('home', {
+			url: "/", //the route
+			templateUrl: "home.html" //the view
+		})
+		.state('about', {
+			url: "/about", //the route
+			templateUrl: "about.html" //the view
+		})
+		.state('test', {
+			url: "/test", //the route
+			templateUrl: "test.html" //the view
+		})
+		// .state('archive', { //chain states to create another route
+		// 	url: "/archive",
+		// 	templateUrl: "archive.html"
+		// });
+
+	$urlRouterProvider.otherwise("/"); //global route. if you go any other url, redirect to '/'
+}
